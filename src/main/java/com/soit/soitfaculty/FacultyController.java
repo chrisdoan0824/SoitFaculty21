b036b219a03628f6bcfa7af6d101b9e892d4f0e2
Chris Doan <doantu@mail.uc.edu>
2021-10-05 14:33:05 -0400

    Updated Controller
    Updated Controller

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -1,10 +1,38 @@
 package com.soit.soitfaculty;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.soit.soitfaculty.model.Faculty;
 
 @Controller
 @RequestMapping("/Faculties")
 public class FacultyController {
+	
+	//Upload faculty info
+	private List<Faculty> thefaculties;
+	
+	
+	@PostConstruct
+	private void loadData() {
+		
+		//Create Faculties
+		Faculty fac1 = new Faculty(1, "Kelly", "Miller", "Kelly@uc.edu");
+		Faculty fac2 = new Faculty(2, "Robert", "Lee", "Robert@uc.edu");
+		Faculty fac3 = new Faculty(3, "Laura", "West", "Laura@uc.edu");
+		
+		//Create our List
+		
+		
+		//Add our List
+		
+	}
+	
+	//Mapping for "/list"
+	
 
 }
